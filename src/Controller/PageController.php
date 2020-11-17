0172155199b18e62dd16a283e07d4a36e222ece4
benoit17630 <benoit.menier@orange.fr>
2020-11-17 14:51:55 +0100

    creation de la homepage et de la metode redirectoroute

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -10,9 +10,16 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class PageController extends AbstractController
 {
+    //creation de la page home
+    /**
+     * @Route("/", name="home")
+     */
+    public function home():Response{
+        return $this->render("index.html.twig");
+    }
     //creation des page blog avec un id en wildcard
     /**
-     * @Route ("/blog/{id}", name="article_page")
+     * @Route ("/blog/{id}", name="blog_page")
      * @param $id
      * @return Response
      */
@@ -26,9 +33,23 @@ class PageController extends AbstractController
             5 => "Article 5",
             6 => "Article 6",
         ];
-// avec la metode Response je peut lier ma function a une page .twig
+// avec la class Response je peut lier ma metode a une page .twig
         return $this->render("blog.html.twig", array(
             "title"=>$articles[$id]
         ));
+    }
+    // redirection apres avoir soumit un formulaire
+    /**
+     * @Route("/form-process", name="form_process")
+     */
+    public function form(){
+        $isformsubmit = true;
+        if (!$isformsubmit){
+            //si le formulaire n est pas soumis
+         return new Response("remplir le formulaire");
+        }else{
+            //si le formulaire est soumis je me redirige sur la page avec le name = home
+           return $this->redirectToRoute("home");
+        }
     }
 }
