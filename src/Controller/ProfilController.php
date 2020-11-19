fd7b1d9cfd095f4def51cdcafbf2f003ef7a79b0
benoit17630 <benoit.menier@orange.fr>
2020-11-19 10:43:12 +0100

    creation de la page et des lien pour envoyer sur le page de chaque agent

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -31,45 +31,99 @@ class ProfilController extends AbstractController
     }
 
 
-    //creation de la page agent
+    //creation de la page agents
     /**
-     * @Route("/agent", name="agent")
+     * @Route("/agents", name="agents")
      */
-    public function agent(){
+    public function agents(){
         $agents = [
             1 => [
+                "id" => 1,
                 "lastName" => "Robert",
                 "firstName" => "David",
                 "age" => 30,
                 "published" => true
             ],
             2 => [
+                "id" => 2,
                 "lastName" => "Labaste",
                 "firstName" => "Denis",
                 "age" => 29,
                 "published" => true
             ],
-            3 => [
+            3 => ["id" => 3,
                 "lastName" => "Rozand",
                 "firstName" => "Mathieu",
                 "age" => 31,
                 "published" => false
             ],
-            4 => [
+            4 => ["id" => 4,
                 "lastName" => "Despert",
                 "firstName" => "Yoann",
                 "age" => 33,
                 "published" => true
             ],
-            5 => [
+            5 => ["id" => 5,
                 "lastName" => "Dorignac",
                 "firstName" => "Loic",
                 "age" => 34,
                 "published" => false
             ]
         ];
-        return $this->render("agent.html.twig",[
+        return $this->render("agents.html.twig",[
             "agents"=> $agents
         ]);
+    }
+
+    //creation de la page agent
+
+    /**
+     * @Route ("/agent/{id}", name="agent_show")
+     */
+    public function agentShow($id){
+        $agents2 = [
+            1 => [
+                "id" => 1,
+                "lastName" => "Robert",
+                "firstName" => "David",
+                "age" => 30,
+                "published" => true
+            ],
+            2 => [
+                "id" => 2,
+                "lastName" => "Labaste",
+                "firstName" => "Denis",
+                "age" => 29,
+                "published" => true
+            ],
+            3 => [
+                "id" => 3,
+                "lastName" => "Rozand",
+                "firstName" => "Mathieu",
+                "age" => 31,
+                "published" => false
+            ],
+            4 => [
+                "id" => 4,
+                "lastName" => "Despert",
+                "firstName" => "Yoann",
+                "age" => 33,
+                "published" => true
+            ],
+            5 => [
+                "id" => 5,
+                "lastName" => "Dorignac",
+                "firstName" => "Loic",
+                "age" => 34,
+                "published" => false
+            ]
+        ];
+
+        $agent =$agents2[$id];
+
+        return $this->render("agent.html.twig",[
+            'agent'=>$agent
+        ]);
+
     }
 }
